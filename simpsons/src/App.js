7569e5e74f40a9c595c2ffc914df70a521530d1e
Doogle <fdunigan11@gmail.com>
2018-09-24 16:48:50 -0400

    Initial working cards

--- simpsons/src/App.js
+++ simpsons/src/App.js
@@ -1,19 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import CharacterCard from "./components/CharacterCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import characters from "./characters.json";
+import "./App.css";
 
 class App extends Component {
+  // Setting this.state.characters to the characters json array
+  state = {
+    characters
+  };
+
+  removecharacter = id => {
+    // Filter this.state.characters for characters with an id not equal to the id being removed
+    const characters = this.state.characters.filter(character => character.id !== id);
+    // Set this.state.characters equal to the new characters array
+    this.setState({ characters });
+  };
+
+  // Map over this.state.characters and render a characterCard component for each character object
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Wrapper>
+        <Title>characters List</Title>
+        {this.state.characters.map(character => (
+          <CharacterCard
+            removecharacter={this.removecharacter}
+            id={character.id}
+            key={character.id}
+            name={character.name}
+            image={character.image}
+            occupation={character.occupation}
+            location={character.location}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
