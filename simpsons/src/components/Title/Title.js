db451bd8ad80f0104d503e80fde27893468793ae
Doogle <fdunigan11@gmail.com>
2018-09-25 16:31:10 -0400

    added background logo

--- simpsons/src/components/Title/Title.js
+++ simpsons/src/components/Title/Title.js
@@ -4,7 +4,7 @@ import "./Title.css";
 const Title = () => (
   <div className="jumbotron jumbotron-fluid">
     <div className="container text-center">
-      <h1 className="display-4">React Clicky Game!</h1>
+      <h1 className="display-4">The Simpsons Clicky Game!</h1>
       <p className="lead">
         Click on any image to earn points, but don't click on any more than
         once!
