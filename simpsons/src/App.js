a3918462f50ceaf18916b011ebf3d32daff38952
Doogle <fdunigan11@gmail.com>
2018-09-26 14:35:59 -0400

    Added simpsons logo and blue styling

--- simpsons/src/App.js
+++ simpsons/src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
-import Jumbotron from "./components/CharacterCard";
+import Title from "./components/Title/Title";
 import Wrapper from "./components/Wrapper";
 import "./App.css";
-import Title from "./components/Title/Title";
-
 
 const App = () => (
   <div className="container-fluid mainContainer">
     <Title />
-    <Wrapper /> 
+    <Wrapper />
   </div>
 );
 
