fd6aab677123579b4c9bddf58843bef022de4a2a
Doogle <fdunigan11@gmail.com>
2018-09-26 16:57:13 -0400

    Added React icon to footer

--- simpsons/src/components/Footer/Footer.js
+++ simpsons/src/components/Footer/Footer.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Footer.css";
+import react from '../Footer/react.png';
 
-const Footer = () => (
-  <footer class="footer">
-    <div class="bottom">Simpsons Clicky Game!</div>
-  </footer>
-);
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer class="footer">
+        <div class="bottom">
+        <img alt="react" src={react}></img>
+          Simpsons Clicky Game!
+        </div>
+      </footer>
+    );
+  }
+}
 
 export default Footer;
--- simpsons/src/components/Title/Title.js
+++ simpsons/src/components/Title/Title.js
@@ -1,16 +1,20 @@
 import React from "react";
 import "./Title.css";
 
-const Title = () => (
-  <div className="jumbotron jumbotron-fluid">
-    <div className="container text-center">
-      <h1 className="display-4">The Simpsons Clicky Game!</h1>
-      <p className="lead">
-        Click on any image to earn points, but click on an image more than once
-        and you lose!
-      </p>
-    </div>
-  </div>
-);
+class Title extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container text-center">
+          <h1 className="display-4">The Simpsons Clicky Game!</h1>
+          <p className="lead">
+            Click on any image to earn points, but click on an image more than
+            once and you lose!
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Title;
